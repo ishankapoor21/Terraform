1b969820484a53e75166a98a96118bd7f2158fc4
ishan_kapoor <64485253+ishankapoor21@users.noreply.github.com>
2021-09-20 12:05:58 +0530

    Add files via upload

---
+++ project.tf
@@ -0,0 +1,52 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.RG
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "network-${var.name}"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.name}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "${var.name}1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "${var.name}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  size               = var.Mtype
+  computer_name  = "${var.name}pc"
+  admin_username = var.name
+  admin_password = "${var.name}K@1230"
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-10"
+    sku       = "20h2-pro"
+    version   = "latest"
+  }
+  os_disk {
+    name              = "${var.name}-disk1"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "Central India"
+}
+
+variable "RG" {
+  default = "ishan-RG"
+}
+
+variable "Mtype" {
+  default = "Standard_F4s_v2"
+}
+
+variable "name" {
+  default = "ishan"
+}
